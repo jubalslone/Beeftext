ebb8ba0f8d43752d21742e603efdd9e4aee2cee0
Jubal Slone <jubal.slone@gmail.com>
2026-07-22 12:01:14 -0500

    Enable restricted TLF build

--- Beeftext/BeeftextConstants.cpp
+++ Beeftext/BeeftextConstants.cpp
@@ -15,6 +15,7 @@ namespace constants {
 
 
 xmilib::VersionNumber const kVersionNumber(16, 0);
+bool const kRestrictedBuild = true;
 QString const kApplicationName = "Beeftext";
 QString const kOrganizationName = "beeftext.org";
 QString const kBeeftextWikiHomeUrl = "https://github.com/xmichelo/Beeftext/wiki";
